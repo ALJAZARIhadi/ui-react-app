608585d912d32e135174eb1532d02ad5c38d23a0
ALJAZARIhadi <hadikezzhe@gmail.com>
2026-09-07 13:48:54 +0300

    Testing Navigations

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import Material from './Material.js'
+import UiAlerts from './UiAlerts.js';
+import UiNavigations from './UiNavigations.js';
+
+
 function App() {
   return (
     <div className="App" style={{marginTop:"300px",marginLeft:"500px"}}>
       <Material/>
+      <UiAlerts/>
+      <UiNavigations/>
     </div>
   );
 }

63dcef114b1a5f16deeae62eb2afd7914e31a5af
ALJAZARIhadi <hadikezzhe@gmail.com>
2026-09-07 14:58:09 +0300

    I used buttons ,chips ,slider and checkboxes and custimized the theme by  createTheme, ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import './App.css';
-import Material from './Material.js'
-import UiAlerts from './UiAlerts.js';
-import UiNavigations from './UiNavigations.js';
-
-
+import "./App.css";
+import Material from "./Material.js";
+/* import UiAlerts from './UiAlerts.js';
+ */
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { orange, green } from "@mui/material/colors";
 function App() {
+  const theme = createTheme({
+    status: {
+      danger: orange[500],
+    },
+    palette: {
+      primary: {
+        main: orange[200],
+      },
+      secondary: {
+        main: green[500],
+      },
+    },
+  });
+
   return (
-    <div className="App" style={{marginTop:"300px",marginLeft:"500px"}}>
-      <Material/>
-      <UiAlerts/>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App" style={{ marginTop: "300px", marginLeft: "500px" }}>
+        <Material />
+        {/* <UiAlerts/> */}
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/Material.js
+++ src/Material.js
@@ -1,16 +1,26 @@
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Chip from "@mimport Checkbox from '@mui/material/Checkbox";
+import Slider from "@mui/material/Slider";
+
 
 export default function Material() {
   return (
-    <Stack direction="row" spacing={2}>
-      <Button color="secondary">Secondary</Button>
-      <Button variant="contained" color="success">
-        Success
-      </Button>
-      <Button variant="outlined" color="error">
-        Error
-      </Button>
-    </Stack>
+    <>
+      <Stack direction="row" spacing={2}>
+        <Button color="secondary">Secondary</Button>
+        <Button variant="contained" color="success">
+          Success
+        </Button>
+        <Button variant="outlined" color="error">
+          Error
+        </Button>
+        {/* <Checkbox {...label} defaultChecked color="secondary" /> */}
+
+        <Chip label="primary" color="primary" variant="outlined" />
+        <Chip label="success" color="success" variant="outlined" />
+        <Slider aria-label="Temperature" defaultValue={30} color="secondary" />
+      </Stack>
+    </>
   );
 }
